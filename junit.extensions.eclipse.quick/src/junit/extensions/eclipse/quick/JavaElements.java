11f2e914e323a6220245acef2259036ee606b0f3
kakutani <kakutani@318e7d8f-da2e-0410-b12c-f8884f942e70>
2008-04-11 15:43:47 +0000

    reorganized tags,branches dirs

---
+++ junit.extensions.eclipse.quick/src/junit/extensions/eclipse/quick/JavaElements.java
@@ -0,0 +1,110 @@
+package junit.extensions.eclipse.quick;
+
+import org.eclipse.jdt.core.Flags;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.ITypeHierarchy;
+import org.eclipse.jdt.core.JavaModelException;
+
+
+public class JavaElements {
+    public static boolean isTestMethod(IJavaElement element) throws JavaModelException {
+        if (!(element instanceof IMethod))
+            return false;
+        IMethod method = (IMethod) element;
+        if (method.getNumberOfParameters() != 0)
+            return false;
+        if (!method.getReturnType().equals("V"))
+            return false;
+        int flags = method.getFlags();
+        if (!Flags.isPublic(flags) || Flags.isStatic(flags))
+            return false;
+        if(method.getElementName().startsWith("test")) return true;
+        
+        return hasTestAnnotationOnMethod(method);
+    }
+
+    public static IType getPrimaryTypeOf(IJavaElement element) {
+        if (element == null)
+            return null;
+        ICompilationUnit cu = null;
+        if (element instanceof ICompilationUnit) {
+            cu = (ICompilationUnit) element;
+        } else if (element instanceof IMember) {
+            cu = ((IMember) element).getCompilationUnit();
+        }
+        return cu != null ? cu.findPrimaryType() : null; 
+    }
+    
+    public static boolean isTestClass(IType type) throws JavaModelException {
+        ITypeHierarchy superTypeHierarchy = type.newSupertypeHierarchy(null);
+        IType superTypes[] = superTypeHierarchy.getAllInterfaces();
+        for (int i = 0; i < superTypes.length; ++i) {
+            IType superType = superTypes[i];
+            if (superType.getFullyQualifiedName().equals(JavaTypes.TEST_INTERFACE_NAME))
+                return true;
+        }
+        return false;
+    }
+
+    public static IJavaElement getTestMethodOrClass(IJavaElement element)
+            throws JavaModelException {
+        while (element != null) {
+            if (isTestMethod(element))
+                return element;
+            
+            if (isTestRunnerPassibleClass(element)) {
+                IType type = (IType) element;
+                if (isTestClass(type)) return element;
+                if (hasSuiteMethod(type)) return element;
+                if (hasTestAnnotation(type)) return element;
+            }
+            element = element.getParent();
+        }
+        return null;
+    }
+
+    private static boolean isTestRunnerPassibleClass(IJavaElement element) throws JavaModelException {
+        if (!(element instanceof IType))
+            return false;
+        IType type = (IType) element;
+        if (!type.isClass())
+            return false;
+        int flags = type.getFlags();
+        if (Flags.isAbstract(flags) || !Flags.isPublic(flags))
+            return false;
+
+        return true;
+    }
+    
+    private static boolean hasSuiteMethod(IType type) throws JavaModelException {
+        IMethod[] methods = type.getMethods();
+        for (int i = 0; i < methods.length; i++) {
+            if (isStaticSuiteMethod(methods[i])) return true;
+        }
+        return false;
+    }
+    
+    private static boolean hasTestAnnotation(IType type) throws JavaModelException{
+        IMethod[] methods = type.getMethods();
+        for (int i = 0; i < methods.length; i++) {
+            if (hasTestAnnotationOnMethod(methods[i])) return true;
+        }
+        return false;
+    }
+    
+    private static boolean hasTestAnnotationOnMethod(IMethod method) throws JavaModelException{
+        return method.getSource() == null ? false:method.getSource().indexOf("@Test") > -1;
+    }
+
+    private static boolean isStaticSuiteMethod(IMethod method) throws JavaModelException {
+        return ((method.getElementName().equals("suite")) &&
+                method.getSignature().equals("()QTest;") &&
+                Flags.isPublic(method.getFlags()) &&
+                Flags.isStatic(method.getFlags())
+        );
+    }
+}
---
+++ junit.extensions.eclipse.quick/src/junit/extensions/eclipse/quick/QuickJUnitPreferencePage.java
@@ -0,0 +1,77 @@
+package junit.extensions.eclipse.quick;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class QuickJUnitPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+
+    private NamingRules namingRules;
+    private NamingRulesPreference namingRulesPreference;
+
+    public QuickJUnitPreferencePage() {
+        setPreferenceStore(QuickJUnitPlugin.getDefault().getPreferenceStore());
+    }
+
+    public void init(IWorkbench workbench)  {
+        IPreferenceStore store = getPreferenceStore();
+        namingRules = new NamingRules(store);
+        namingRulesPreference = new NamingRulesPreference(this);
+	}
+
+	protected Control createContents(Composite parent)  {
+        // TODO implements source folder feature.
+//	    Composite sourceFolderComposite = new Composite(parent, SWT.NULL);
+//	    GridLayout sfLayout = new GridLayout(2, false);
+//        sfLayout.marginHeight = 0;
+//        sfLayout.marginWidth = 0;
+//	    sourceFolderComposite.setLayout(sfLayout);
+//	    GridData sfGData = new GridData(GridData.FILL_HORIZONTAL);
+//	    sourceFolderComposite.setLayoutData(sfGData);
+//	    Label label = new Label(sourceFolderComposite, SWT.NONE);
+//	    label.setText("testCase source folder;");
+//	    Text text = new Text(sourceFolderComposite, SWT.BORDER);
+//	    GridData textGData = new GridData(GridData.FILL_HORIZONTAL);
+//	    text.setLayoutData(textGData);
+//	    text.setText("${project}/src");
+
+        Composite composite= new Composite(parent, SWT.NULL);
+        GridLayout layout= new GridLayout();
+        layout.numColumns= 1;
+        layout.marginWidth= 0;
+        composite.setLayout(layout);
+        GridData data= new GridData(GridData.FILL_BOTH);
+        composite.setLayoutData(data);
+
+        
+        namingRulesPreference.create(namingRules.get(), composite);
+        Dialog.applyDialogFont(composite);
+        return composite;
+	}
+
+    GridData getButtonGridData(Button button) {
+        GridData gd= new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_BEGINNING);
+        int widthHint= convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH);
+        gd.widthHint= Math.max(widthHint, button.computeSize(SWT.DEFAULT, SWT.DEFAULT, true).x);
+        return gd;
+    }
+
+    protected void performDefaults() {
+        super.performDefaults();
+        namingRulesPreference.setValue(namingRules.getDefault());
+    }
+
+    public boolean performOk() {
+        namingRules.set(namingRulesPreference.getValue());
+        return true;
+    }
+}
