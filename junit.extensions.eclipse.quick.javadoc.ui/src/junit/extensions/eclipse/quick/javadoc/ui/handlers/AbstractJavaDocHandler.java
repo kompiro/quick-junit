89f43fd38429a5bb2a669c979ae65fb9571e034b
kompiro <kompiro@318e7d8f-da2e-0410-b12c-f8884f942e70>
2009-03-27 09:01:26 +0000

    プラグインのバージョンの指定を外しました。 カーソル以下のエレメントを取得するようにしました。

---
+++ junit.extensions.eclipse.quick.javadoc.ui/src/junit/extensions/eclipse/quick/javadoc/ui/handlers/AbstractJavaDocHandler.java
@@ -0,0 +1,80 @@
+package junit.extensions.eclipse.quick.javadoc.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+public abstract class AbstractJavaDocHandler extends AbstractHandler {
+
+	private ITextEditor textEditor;
+	private IWorkbenchWindow window;
+
+	public final Object execute(ExecutionEvent event) throws ExecutionException {
+		window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		textEditor = (ITextEditor)HandlerUtil.getActiveEditor(event);
+		return doExecute(event);
+	}
+	
+	protected abstract Object doExecute(ExecutionEvent event) throws ExecutionException;
+	
+	protected ITextEditor getTextEdtior(){
+		return textEditor;
+	}
+	
+	protected IWorkbenchWindow getWorkbenchWindow(){
+		return window;
+	}
+	
+    protected IJavaElement getElementOfJavaEditor() throws JavaModelException {
+        ICompilationUnit unit = getCompilationUnitOfJavaEditor();
+        if (unit == null)
+            return null;
+        ITextSelection text = getTextSelectionOfJavaEditor();
+		int offset = text.getOffset();
+        IJavaElement element = unit.getElementAt(offset);
+        return element;
+    }
+    
+    protected IJavaElement getElementOfCurrentCursor() throws JavaModelException{
+        ICompilationUnit unit = getCompilationUnitOfJavaEditor();
+        if (unit == null)
+            return null;
+        ITextSelection text = getTextSelectionOfJavaEditor();
+		int offset = text.getOffset();
+		int length = text.getLength();
+        IJavaElement[] elements = unit.codeSelect(offset, length);
+        return elements[0];
+    }
+    
+    private ITextSelection getTextSelectionOfJavaEditor() throws JavaModelException {
+        ISelectionProvider provider = getTextEdtior().getSelectionProvider();
+        ISelection selection = provider.getSelection();
+        if (!(selection instanceof ITextSelection))
+            return null;
+        return (ITextSelection) selection;
+    }
+        
+    private ICompilationUnit getCompilationUnitOfJavaEditor() throws JavaModelException {
+        ITextEditor textEdtior = getTextEdtior();
+		if (textEdtior == null)
+            return null;
+        IEditorInput input = textEdtior.getEditorInput();
+        IJavaElement element = (IJavaElement) input.getAdapter(IJavaElement.class);
+        if (element instanceof ICompilationUnit)
+            return (ICompilationUnit) element;
+        return null;
+    }
+
+
+
+}
--- junit.extensions.eclipse.quick.javadoc.ui/src/junit/extensions/eclipse/quick/javadoc/ui/handlers/CreateSeeTagHandler.java
+++ junit.extensions.eclipse.quick.javadoc.ui/src/junit/extensions/eclipse/quick/javadoc/ui/handlers/CreateSeeTagHandler.java
@@ -1,34 +1,37 @@
 package junit.extensions.eclipse.quick.javadoc.ui.handlers;
 
-import org.eclipse.core.commands.AbstractHandler;
+import junit.extensions.eclipse.quick.javadoc.ui.JavaDocUIActivator;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.IDocument;
 
-/**
- * Our sample handler extends AbstractHandler, an IHandler base class.
- * @see org.eclipse.core.commands.IHandler
- * @see org.eclipse.core.commands.AbstractHandler
- */
-public class CreateSeeTagHandler extends AbstractHandler {
-	/**
-	 * The constructor.
-	 */
+public class CreateSeeTagHandler extends AbstractJavaDocHandler {
+
 	public CreateSeeTagHandler() {
 	}
 
-	/**
-	 * the command has been executed, so extract extract the needed information
-	 * from the application context.
-	 */
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"Quick JUnit JavaDoc UI Plug-in",
-				"Create See Tag");
+	public Object doExecute(ExecutionEvent event) throws ExecutionException {
+		try {
+			IJavaElement elementOnCursor = getElementOfCurrentCursor();
+			IJavaElement elementOfJavaEditor = getElementOfJavaEditor();
+			if(elementOfJavaEditor instanceof IMethod || elementOfJavaEditor instanceof IType){
+				IMember member = (IMember) elementOfJavaEditor;
+			}
+//			MessageDialog.openInformation(
+//					getWorkbenchWindow().getShell(),
+//					"Quick JUnit JavaDoc UI Plug-in",
+//			"element:" + elementOnCursor);
+		} catch (JavaModelException e) {
+			JavaDocUIActivator.getDefault().handleSystemError(e, this);
+		}
 		return null;
 	}
+
 }
