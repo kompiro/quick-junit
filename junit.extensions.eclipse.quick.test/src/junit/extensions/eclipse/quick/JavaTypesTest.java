f09ea0d47176fd6f49b4c7b6de0560bfc1f57704
Hiroki Kondo <kompiro@gmail.com>
2010-11-16 01:30:49 +0900

    added util test

---
+++ junit.extensions.eclipse.quick.test/src/junit/extensions/eclipse/quick/JavaTypesTest.java
@@ -0,0 +1,41 @@
+package junit.extensions.eclipse.quick;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class JavaTypesTest {
+	
+	@Test
+	public void not_test_class_should_return_false() throws Exception {
+		
+		IType result = new ITypeMockBuilder().build();
+		assertThat(JavaTypes.isTest(result),is(false));
+		
+	}
+	
+	@Ignore
+	@Test
+	public void junit3_class_should_return_true() throws Exception {
+		
+		IType result = new ITypeMockBuilder().junit3_class().build();
+		assertThat(JavaTypes.isTest(result),is(true));
+		
+	}
+	
+	@Ignore
+	@Test
+	public void junit4_class_should_return_true() throws Exception {
+		
+		IMethod method = new IMethodMockBuilder().addTestAnnotation().build();
+		IType result = new ITypeMockBuilder().addMethod(method ).build();
+		assertThat(JavaTypes.isTest(result),is(true));
+		
+	}
+
+}
