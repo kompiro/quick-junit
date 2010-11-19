db1ce85f9c2289a36f42d49c1ccb908dbff74217
Hiroki Kondo <kompiro@gmail.com>
2010-11-19 23:04:56 +0900

    separated preference initializer for favorite static imports.

--- junit.extensions.eclipse.quick.mock/src/junit/extensions/eclipse/quick/mock/internal/preference/PreferenceInitializer.java
+++ junit.extensions.eclipse.quick.mock/src/junit/extensions/eclipse/quick/mock/internal/preference/PreferenceInitializer.java
@@ -7,26 +7,21 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import junit.extensions.eclipse.quick.mock.internal.Activator;
-
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jdt.ui.PreferenceConstants;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
-import org.eclipse.ui.IStartup;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
 
 /**
  * Initializes the default preferences if none exist.
  * 
  */
-public class PreferenceInitializer extends AbstractPreferenceInitializer implements IStartup, IPropertyChangeListener {
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
 	private static final String		EMPTY										= "";											//$NON-NLS-1$
 	private static final String		SEMI_COLON									= ";";											//$NON-NLS-1$
 
-	private final IPreferenceStore	quickjunitPreferenceStore;
 	private final IPreferenceStore	jdtPreferenceStore;
 	static final String				ENABLE_ADDITIONAL_AUTOCOMPLETE_FAVOURTES	= "ENABLE_ADDITIONAL_AUTOCOMPLETE_FAVOURTES";	//$NON-NLS-1$
 
@@ -34,39 +29,27 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer impleme
 	 * Creates a default preference initializer.
 	 */
 	public PreferenceInitializer() {
-		this(Activator.getDefault().getPreferenceStore(), PreferenceConstants.getPreferenceStore());
+		this(PreferenceConstants.getPreferenceStore());
 	}
 
 	/**
 	 * Create a preference initializer with the two preference stores.
 	 * 
-	 * @param quickjunitPreferenceStore used by quick junit.
 	 * @param jdtPreferenceStore used by JDT.
 	 */
-	public PreferenceInitializer(IPreferenceStore quickjunitPreferenceStore, IPreferenceStore jdtPreferenceStore) {
-		this.quickjunitPreferenceStore = quickjunitPreferenceStore;
+	public PreferenceInitializer(IPreferenceStore jdtPreferenceStore) {
 		this.jdtPreferenceStore = jdtPreferenceStore;
-		quickjunitPreferenceStore.addPropertyChangeListener(this);
 	}
 
 	public void initializeDefaultPreferences() {
-		quickjunitPreferenceStore.setDefault(PreferenceInitializer.ENABLE_ADDITIONAL_AUTOCOMPLETE_FAVOURTES, true);
-	}
-
-	public void earlyStartup() {
-		new PreferenceInitializer().initializeFavorites();
+		initializeFavorites();
 	}
 
 	void initializeFavorites() {
 		Set<String> imports;
-		if (quickjunitPreferenceStore.getBoolean(PreferenceInitializer.ENABLE_ADDITIONAL_AUTOCOMPLETE_FAVOURTES)) {
-			imports = new LinkedHashSet<String>(getJDTImports());
-			imports.addAll(getDefaultFavorites());
-		} else {
-			imports = getJDTImports();
-			if (imports.containsAll(getDefaultFavorites()))
-				imports.removeAll(getDefaultFavorites());
-		}
+		imports = getJDTImports();
+		if (imports.containsAll(getDefaultFavorites()))
+			imports.removeAll(getDefaultFavorites());
 		String join = join(imports, SEMI_COLON);
 		jdtPreferenceStore.setValue(PreferenceConstants.CODEASSIST_FAVORITE_STATIC_MEMBERS, join);
 		try {
@@ -76,7 +59,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer impleme
 	}
 
 	/**
-	 * @return the JDT favourite imports.
+	 * @return the JDT favorite imports.
 	 */
 	public Set<String> getJDTImports() {
 		String preference = jdtPreferenceStore.getString(PreferenceConstants.CODEASSIST_FAVORITE_STATIC_MEMBERS);
@@ -89,11 +72,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer impleme
 
 	private LinkedHashSet<String> getDefaultFavorites() {
 		LinkedHashSet<String> orderedSet = new LinkedHashSet<String>();
-		orderedSet.add(importStatement("org.mockito.Matchers"));
 		orderedSet.add(importStatement("org.mockito.Mockito"));
-		orderedSet.add(importStatement("org.hamcrest.Matchers"));
-		orderedSet.add(importStatement("org.hamcrest.MatcherAssert"));
-		orderedSet.add(importStatement("org.junit.Assert"));
 		return orderedSet;
 	}
 
